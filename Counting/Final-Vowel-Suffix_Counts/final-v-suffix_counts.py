067e04469eee771fe230e389812c80fe9c931d05
Selinalkan <alkan.selin@gmail.com>
2023-06-22 21:31:48 -0400

    counts into two files

--- Counting/Final-Vowel-Suffix_Counts/final-v-suffix_counts.py
+++ Counting/Final-Vowel-Suffix_Counts/final-v-suffix_counts.py
@@ -2,7 +2,7 @@
 
 """This script counts stem-final vowels and final-vowel & passive-suffix pairs attached to verbs in
      Maori.The verbs going through various internal changes are not accounted
-     for in this script."""
+     for in this script. The counts are written into two separate tsv files."""
 
 import argparse
 import collections
@@ -42,20 +42,21 @@ suffix_dict = {
 def main(args: argparse.Namespace) -> None:
     final_vowel_counts = collections.Counter()
     final_v_suffix_counts = collections.Counter()
-    with open(args.input, "r") as source, open(args.output, "w") as sink:
+    with open(args.input, "r") as source, open(args.output1, "w") as sink1, open(args.output2, "w") as sink2:
         tsv_reader = csv.reader(source, delimiter="\t")
-        tsv_writer = csv.writer(sink, delimiter="\t")
+        tsv_writer1 = csv.writer(sink1, delimiter="\t")
+        tsv_writer2 = csv.writer(sink2, delimiter="\t")
         for lemma, suffix in tsv_reader:
             for vowel in vowel_dict:
                 if lemma.endswith(vowel):
                     final_vowel_counts[vowel] += 1
                     final_v_suffix_counts[(vowel, suffix)] += 1
         for vowel, count in final_vowel_counts.most_common():
-            tsv_writer.writerow(
+            tsv_writer1.writerow(
                 [vowel, count]
             )  # need to give a list as an argument
         for (vowel, suffix), count in final_v_suffix_counts.most_common():
-            tsv_writer.writerow([vowel, suffix, count])
+            tsv_writer2.writerow([vowel, suffix, count])
             print(f"{vowel}:\t{count}")
 
 
@@ -63,6 +64,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", required=True, help="input Maori TSV file")
     parser.add_argument(
-        "--output", required=True, help="output stem-final vowels with counts"
+        "--output1", required=True, help="output stem-final vowels and vowel-suffix pairs with counts"
     )
+    parser.add_argument("--output2", required=True, help="outputs the vowel-suffix counts")
     main(parser.parse_args())
