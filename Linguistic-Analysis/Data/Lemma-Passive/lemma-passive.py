6ea30e47a66128d0069d367a5233329210c4be50
Selinalkan <alkan.selin@gmail.com>
2023-08-01 09:02:36 -0400

    Added /Data, /Sorted, and /BK-tree

---
+++ Linguistic-Analysis/Data/Lemma-Passive/lemma-passive.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# For some reason, ./ execution gives me "env: python\r: No such file or directory error"
+# but I could run the file with "python Maori...py"
+# there's something not working in this file, though, since it works with others.
+
+"""This script splits the Maori verbs with passive suffixes into two columns
+        the first of which with the uninflected lemmas and the second of which
+        with the unhyphenated inflected forms. The verbs going through internal
+        change have not been included in this data set."""
+
+import argparse
+import re
+import csv
+
+
+def main(args: argparse.Namespace) -> None:
+    with open(args.input, "r") as source, open(args.output, "w") as sink:
+        tsv_reader = csv.reader(source, delimiter="\t")
+        tsv_writer = csv.writer(sink, delimiter="\t")
+        for row in tsv_reader:
+            match_obj = re.match(r"(\w+)(\-)(\w+)", row[1])
+            if match_obj:
+                row = [
+                    match_obj.group(1),
+                    match_obj.group(1) + match_obj.group(3),
+                ]
+            assert row[0] != ""
+            tsv_writer.writerow(row)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", default="BR_8-1-23.tsv", help="input Maori file")
+    parser.add_argument(
+        "--output", default="mri-lemma-passive.tsv", help="output Maori file as lemmas"
+    )
+    main(parser.parse_args())
+
---
+++ Linguistic-Analysis/Phonological-Neighbor/bk-tree.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+"""Demonstration using B-K trees for nearest neighbor computation.
+The Wikipedia page has a good introduction:
+    https://en.wikipedia.org/wiki/BK-tree
+"""
+
+from typing import Any, List
+
+import numpy
+
+# Found here:
+#     https://github.com/benhoyt/pybktree
+# and on PyPI (i.e. via pip).
+import pybktree
+import argparse
+
+DATA = [
+    "animus",
+    "melodious",
+    "Tupian",
+    "turnscrew" "acidyl",
+    "allheal",
+    "handsome",
+    "suaharo",
+    "Muhlenbergia",
+    "Choluteca",
+    "Alouatta",
+    "submetering",
+    "shoppy",
+    "chillroom",
+    "Nisaean",
+    "olefin",
+    "arthrosporic",
+    "conjugable",
+    "anacephalize",
+    "prozone",
+]
+
+# If there are no candidates within this bound we error out so this doesn't
+# go on infinitely.
+UPPER_BOUND = 10
+
+
+class Error(Exception):
+    pass
+
+
+def edit_distance(x: List[Any], y: List[Any]) -> int:
+    # For a more expressive version of the same, see:
+    #
+    #     https://gist.github.com/kylebgorman/8034009
+    idim = len(x) + 1
+    jdim = len(y) + 1
+    table = numpy.zeros((idim, jdim), dtype=numpy.uint8)
+    table[1:, 0] = 1
+    table[0, 1:] = 1
+    for i in range(1, idim):
+        for j in range(1, jdim):
+            if x[i - 1] == y[j - 1]:
+                table[i][j] = table[i - 1][j - 1]
+            else:
+                c1 = table[i - 1][j]
+                c2 = table[i][j - 1]
+                c3 = table[i - 1][j - 1]
+                table[i][j] = min(c1, c2, c3) + 1
+    return int(table[-1][-1])
+
+
+def main() -> None:
+    # We pass the distance metric and the data during construction.
+    tree = pybktree.BKTree(edit_distance, DATA)
+    # The author has not provided us a nearest neighbor function. What
+    # they have provided us with is a `find` method which gives us all
+    # neighbors within a certain distance. By slowly increasing this until this
+    # set is non-empty we can find the nearest neighbors. Sicne there may be
+    # more than one, we return them all.
+    lookup = "prozone"
+    found_something = False
+    for bound in range(1, 1 + UPPER_BOUND):
+        found = tree.find(lookup, bound)
+        if len(found) > 1:
+            found_something = True
+        else:
+            continue
+        for distance, match in found:
+            # We have to take special care not to return a word as its own
+            # nearest neighbor.
+            if distance == 0:
+                continue
+            else:
+                print(f"Nearest neigbhor: {match}")
+        break
+    if not found_something:
+        raise Error(f"no match found within {UPPER_BOUND} edits")
+
+
+if __name__ == "__main__":
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument(
+    #     "-i",
+    #     "--input",
+    #     default="lemma-suffix_only.tsv",
+    #     help="input Maori TSV file",
+    # )
+    # parser.add_argument(
+    #     "-o1",
+    #     "--output1",
+    #     default="00_bktree_Kyle.tsv",
+    #     help="outputs stem-final vowel counts",
+    # )
+    # main(parser.parse_args())
+    main()
